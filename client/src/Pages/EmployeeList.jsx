9bf526061b66dc72b489a78c79276c33f9e5bce1
Andra0011 <capraruandra@gmail.com>
2023-05-02 17:50:04 +0300

    add filter and sort

--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -15,6 +15,7 @@ const deleteEmployee = (id) => {
 const EmployeeList = () => {
   const [loading, setLoading] = useState(true);
   const [employees, setEmployees] = useState(null);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
   const handleDelete = (id) => {
     deleteEmployee(id);
@@ -24,10 +25,48 @@ const EmployeeList = () => {
     });
   };
 
+  const handleInput = (event) => {
+    setFilteredEmployees(employees.filter((employee) => employee.position.toLowerCase().includes(event.target.value) || employee.level.toLowerCase().includes(event.target.value)));
+  };
+
+  const handleSortFN = () => {
+    let sortFN = [...filteredEmployees].sort((a,b) => a.name > b.name ? 1 : -1 )
+    setFilteredEmployees(sortFN);
+  };
+
+  const handleSortP = () => {
+    let sortP = [...filteredEmployees].sort((a,b) => a.position > b.position ? 1 : -1 )
+    setFilteredEmployees(sortP);
+  };
+
+  const handleSortLN = () => {
+    let sortFN = [...filteredEmployees].sort((a,b) => a.name.split(" ")[a.name.split(" ").length-1] > b.name.split(" ")[b.name.split(" ").length-1] ? 1 : -1 )
+    setFilteredEmployees(sortFN);
+  };
+ const handleSortL = () => {
+  let sortL = [...filteredEmployees].sort((a,b) => a.level > b.level ? 1 : -1 )
+  setFilteredEmployees(sortL);
+ };
+
+  const handleSortMN = () => {
+    let sortMN = [...filteredEmployees]
+    sortMN = sortMN.sort((a,b) => {
+    if(a.name.split(" ").length == 3 ){
+      if(b.name.split(" ").length == 3){
+        if(a.name.split(" ")[1] > b.name.split(" ")[1]){
+          return 1
+        } else return -1
+      } else return -1
+    } else return 1
+    })
+    setFilteredEmployees(sortMN);
+  };  
+
   useEffect(() => {
     fetchEmployees()
       .then((employees) => {
         setLoading(false);
+        setFilteredEmployees(employees);
         setEmployees(employees);
       })
   }, []);
@@ -36,7 +75,17 @@ const EmployeeList = () => {
     return <Loading />;
   }
 
-  return <EmployeeTable employees={employees} onDelete={handleDelete} />;
+  return (<>
+    <input type="text" onChange={handleInput} placeholder="search position or level" />
+    <button onClick={handleSortFN}>Sort by First Name</button>
+    <button onClick={handleSortMN}>Sort by Middle Name</button>
+    <button onClick={handleSortLN}>Sort by Last Name</button>
+    <button onClick={handleSortP}>Sort by Position</button>
+    <button onClick={handleSortL}>Sort by Level</button>
+    <EmployeeTable employees={filteredEmployees} onDelete={handleDelete} />;
+  </>
+
+  )
 };
 
 export default EmployeeList;
